acd054ebd84ab5eab64291a20c1328cc7c782fa9
PEDRO RIVERA <riverv83500@gmail.com>
2022-05-18 15:58:28 -0400

    second commit

--- modelos/clientes.js
+++ modelos/clientes.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose')
     , Schema = mongoose.Schema
 
-mongoose.connect(process.env.DB_MONGO);
+mongoose.connect(process.env.DB_MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(db =>{
+    
+    console.log("Db is connected a", process.env.DB_MONGO)
+    console.log("Db is connected", db.connection.host)
+})
+    .catch(err => console.log(err));
+    
 //mongoose.connect('mongodb://superAdmin:pass1234@167.99.2.159:27017/sulem?authSource=admin', { useNewUrlParser: true });
 var clientes = Schema({
     nombre: String,
--- modelos/tipoCambio.js
+++ modelos/tipoCambio.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose')
     , Schema = mongoose.Schema
 
-mongoose.connect(process.env.DB_MONGO)
-    .then(db => console.log("Db is connected", db.connection.host))
+mongoose.connect(process.env.DB_MONGO, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+    .then(db => {
+    
+        console.log("Db is connected a", process.env.DB_MONGO)
+        console.log("Db is connected", db.connection.host)
+    })
     .catch(err => console.log(err));
 //mongoose.connect('mongodb://superAdmin:pass1234@167.99.2.159:27017/sulem?authSource=admin', { useNewUrlParser: true });
 var tipoCambio = Schema({
